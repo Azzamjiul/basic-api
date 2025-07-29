b245f8aa7f4d26fb71337cf15ce757f1fa3bd44e
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2025-07-30 05:55:38 +0700

    feat: add x_token header to hello endpoint

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from app.settings import settings
 from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI(title=settings.APP_NAME, version=settings.VERSION)
 
 @app.get("/")
-def hello(max_price: Optional[int] = None):
+def hello(max_price: Optional[int] = None, x_token: str = Header(None)):
     return {
         "message": "Hello, World!",
-        "max_price": max_price
+        "max_price": max_price,
+        "x_token": x_token
     }
 
 @app.get("/scalar", include_in_schema=False)
